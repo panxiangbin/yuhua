939b1e8c561bba2657a11539d55d3289d67decb7
潘斌 <smxpxb008@gmail.com>
2026-07-28 09:49:45 +0800

    chore: add centralized Yuhua contact configuration

---
+++ assets/site-config.js
@@ -0,0 +1,9 @@
+// 予华仪器网站统一配置
+// 销售联系方式变化时，只修改本文件，不要在各页面重复替换。
+window.YUHUA_SITE = {
+  company: "巩义市予华仪器有限责任公司",
+  phone: "15517593858",
+  wechat: "15517593858",
+  address: "巩义市英峪工业区",
+  officialSite: "https://www.gyyuhua.cn/"
+};
